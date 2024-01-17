5c65b18fd6d38352458bc001ef9e128dd567a121
lisuccia20 <lisa.manoni2@gmail.com>
2024-01-17 10:22:18 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Pokemons from "./pages/Pokemons/Pokemons"
+import PokemonDetails from "./pages/PokemonDetails/PokemonDetails"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element = {<Pokemons/>}/>
+        <Route path="/details" element = {<PokemonDetails/>}/>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/pages/PokemonDetails/PokemonDetails.jsx
@@ -0,0 +1,7 @@
+export default function PokemonDetails(){
+    return(
+        <>
+        Sono PokemonDetails
+        </>
+    )
+}
